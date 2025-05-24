1bd6fb1d14bb6c8ff492c7b1cbb06fcd22a8ede7
Natalie17-JS <Kassandra8801@outlook.com>
2025-05-24 13:38:05 +0200

    nuxt begin

---
+++ components/Homepage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home-container">
+    <!-- Верхняя полоса с логотипом -->
+    <div class="logo-bar">
+      <img src="/logo.svg" alt="logo" class="logo-image" />
+    </div>
+
+    <!-- Центральная часть: окно и дверь -->
+    <div class="middle-section">
+
+        <div>
+         <img src="/plant.svg" alt="plant" class="plant-image" />
+      <div class="window"></div>
+       <img src="/plant.svg" alt="plant" class="plant-image" />
+       </div>
+
+      <div class="door">
+        <img src="/door.svg" alt="door" class="door-image" />
+      </div>
+    </div>
+
+    <!-- Нижняя часть: дорога -->
+    <div class="road"></div>
+  </div>
+</template>
+
+
+<style scoped>
+.logo-bar{
+    width: 100vw;
+    height: 30vh;
+    background-color: coral;
+}
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image']
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
+   css: [
+    '~/assets/css/global.css'
+  ]
 })
